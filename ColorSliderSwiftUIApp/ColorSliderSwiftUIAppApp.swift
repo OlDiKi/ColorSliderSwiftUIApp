2173bf88473613e975c5d84661482cc7c9320273
OlDiKi <delorian101allgood@gmail.com>
2022-02-19 15:15:48 +0300

    Initial Commit

---
+++ ColorSliderSwiftUIApp/ColorSliderSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliderSwiftUIAppApp.swift
+//  ColorSliderSwiftUIApp
+//
+//  Created by Дмитрий Олейнер on 19.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliderSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
