468be351a889e4c9cc33b0da53497d8f14a5a6ff
OlDiKi <delorian101allgood@gmail.com>
2022-02-19 16:19:38 +0300

    1. Create TextFieldView

--- ColorSliderSwiftUIApp/TextFieldView.swift
+++ ColorSliderSwiftUIApp/TextFieldView.swift
@@ -8,13 +8,26 @@
 import SwiftUI
 
 struct TextFieldView: View {
+    
+    @Binding var text: String
+    @Binding var value: Double
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TextField("", text: $text)
+            .frame(width: 60, alignment: .center)
+            .multilineTextAlignment(.center)
+            .overlay(
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(.blue)
+            )
+            .shadow(color: Color.gray, radius: 3, x: 1, y: 2)
+        
     }
 }
 
 struct TextFieldView_Previews: PreviewProvider {
     static var previews: some View {
-        TextFieldView()
+        TextFieldView(text: .constant("85"), value: .constant(85))
     }
 }
