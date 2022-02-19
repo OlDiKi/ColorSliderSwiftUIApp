85cabf0bfea066588906b279a1a7914231af743d
OlDiKi <delorian101allgood@gmail.com>
2022-02-19 19:04:34 +0300

    1.Setup  ColorView and SliderView on ContentView

--- ColorSliderSwiftUIApp/ContentView.swift
+++ ColorSliderSwiftUIApp/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var red = Double.random(in: 0...255)
+    @State private var green = Double.random(in: 0...255)
+    @State private var blue = Double.random(in: 0...255)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack(spacing: 30) {
+            ColorView(red: red, green: green, blue: blue)
+            
+            VStack {
+                SliderView(value: $red, color: .red)
+                SliderView(value: $green, color: .green)
+                SliderView(value: $blue, color: .blue)
+            }
+            .frame(height: 150)
+            Spacer()
+        }
     }
 }
 
